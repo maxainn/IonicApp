8ab52e4a7b5bbc3027c656c513addb03aa7a7637
Ain <maxain201@gmail.com>
2017-07-13 23:30:56 -0300

    Import Api Code

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+    selector: 'page-about',
+    templateUrl: 'about.html'
 })
 export class AboutPage {
+peliculas: any;
+    constructor(public navCtrl: NavController, public http: Http) {
+        console.log('paso 1');
+        this.http.get('http://swapi.co/api/people/?page=2')
+            .map(res => res.json())
+            .subscribe(data => {
+                console.log('paso 3');
+                console.log(data);
+                this.peliculas = data.results;
+            });
+    }
 
-  constructor(public navCtrl: NavController) {
 
-  }
+
 
 }
